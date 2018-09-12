e776f1da5578efd67cef4599ebffa139dff462d9
liujianjun <liujianjun@togeek.cn>
2018-09-12 18:40:54 +0800

    更新代码

--- src/com/nii/desktop/adb/DeviceChangeListener.java
+++ src/com/nii/desktop/adb/DeviceChangeListener.java
@@ -6,8 +6,7 @@ import com.android.ddmlib.IDevice;
 /**
  * Created by wzj on 2017/8/21.
  */
-public class DeviceChangeListener implements AndroidDebugBridge.IDeviceChangeListener
-{
+public class DeviceChangeListener implements AndroidDebugBridge.IDeviceChangeListener {
     /**
      * Sent when the a device is connected to the {@link AndroidDebugBridge}.
      * <p>
@@ -16,8 +15,7 @@ public class DeviceChangeListener implements AndroidDebugBridge.IDeviceChangeLis
      * @param device the new device.
      */
     @Override
-    public void deviceConnected(IDevice device)
-    {
+    public void deviceConnected(IDevice device) {
         System.out.println("Device connect " + device.getSerialNumber());
     }
 
@@ -29,30 +27,27 @@ public class DeviceChangeListener implements AndroidDebugBridge.IDeviceChangeLis
      * @param device the new device.
      */
     @Override
-    public void deviceDisconnected(IDevice device)
-    {
+    public void deviceDisconnected(IDevice device) {
         System.out.println("Device disconnect " + device.getSerialNumber());
     }
 
     /**
-     * Sent when a device data changed, or when clients are started/terminated on the device.
+     * Sent when a device data changed, or when clients are started/terminated on
+     * the device.
      * <p>
      * This is sent from a non UI thread.
      *
      * @param device     the device that was updated.
-     * @param changeMask the mask describing what changed. It can contain any of the following
-     *                   values: {@link IDevice#CHANGE_BUILD_INFO}, {@link IDevice#CHANGE_STATE},
+     * @param changeMask the mask describing what changed. It can contain any of the
+     *                   following values: {@link IDevice#CHANGE_BUILD_INFO},
+     *                   {@link IDevice#CHANGE_STATE},
      *                   {@link IDevice#CHANGE_CLIENT_LIST}
      */
     @Override
-    public void deviceChanged(IDevice device, int changeMask)
-    {
-        if (device.isOnline())
-        {
+    public void deviceChanged(IDevice device, int changeMask) {
+        if (device.isOnline()) {
             System.out.println("Device change online " + device.getSerialNumber());
-        }
-        else
-        {
+        } else {
             System.out.println("Device change offline " + device.getSerialNumber());
         }
     }
